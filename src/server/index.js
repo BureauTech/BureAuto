ae52b415fbd83e1206b1beaa963683e336aebdd6
WesVisiona <wesley.dias@visionaespacial.com.br>
2021-09-01 08:27:15 -0300

    Adição csv parser

--- src/server/index.js
+++ src/server/index.js
@@ -1,28 +1,28 @@
 // Importações
 require("dotenv").config()
-const express    = require("express")
-const cors       = require("cors")
+const express = require("express")
+const cors = require("cors")
 
 // Invocação da aplicação
 const app = express()
 
 // Definições iniciais do APP
-    app.use(cors())
-    app.use(express.urlencoded({limit: "50mb", extended: true, parameterLimit: 50000}))
-    app.use(express.json({limit: "50mb"}))
+app.use(cors())
+app.use(express.urlencoded({limit: "50mb", extended: true, parameterLimit: 50000}))
+app.use(express.json({limit: "50mb"}))
 
 // Definição dos controllers
-    app.use("/adverts", require("./controllers/AdvertsController"))
-    app.use("/bookmarks", require("./controllers/BookmarksController"))
-    app.use("/mensagens", require("./controllers/MessagesController"))
-    app.use("/readUsers", require("./controllers/ReadUsersController"))
-    app.use("/users", require("./controllers/UsersController"))
-    app.use("/views", require("./controllers/ViewsController"))
+app.use("/adverts", require("./controllers/AdvertsController"))
+app.use("/bookmarks", require("./controllers/BookmarksController"))
+app.use("/mensagens", require("./controllers/MessagesController"))
+app.use("/readUsers", require("./controllers/ReadUsersController"))
+app.use("/users", require("./controllers/UsersController"))
+app.use("/views", require("./controllers/ViewsController"))
 
 // Rota inexistente
-    app.use((req, res) => {
-        res.send("Requisição não encontrada.")
-    })
+app.use((req, res) => {
+    res.send("Requisição não encontrada.")
+})
 
 const port = process.env.PORT || 3000
 
