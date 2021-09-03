e4fc0df5e67f0a48cd0d7c5b530ed086921f2d7c
WesVisiona <wesley.dias@visionaespacial.com.br>
2021-09-03 09:07:53 -0300

    Alterações do  estilo do codigo

--- src/server/services/EmailService.js
+++ src/server/services/EmailService.js
@@ -1,17 +1,15 @@
-const nodemailer = require("nodemailer")
 const ejs = require("ejs")
+const nodemailer = require("nodemailer")
 
 /* eslint-disable-next-line no-unused-vars */
 const EmailService = module.exports = {
+    
     transporter: nodemailer.createTransport({
         service: process.env.EMAIL_SERVICE,
         host: process.env.EMAIL_HOST,
         port: Number(process.env.EMAIL_PORT),
         secure: process.env.EMAIL_SECURE === "true",
-        auth: {
-            user: process.env.EMAIL_USER,
-            pass: process.env.EMAIL_PASSWORD
-        }
+        auth: {user: process.env.EMAIL_USER, pass: process.env.EMAIL_PASSWORD}
     }),
 
     sendEmail: async function(from, to, subject, template, data) {
