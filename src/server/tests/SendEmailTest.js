ce98bb4cc87b8d8497880b00237635362eb83fd2
WesVisiona <wesley.dias@visionaespacial.com.br>
2021-09-03 12:33:20 -0300

    Renomeação de template e ajustes de formatação

--- src/server/tests/SendEmailTest.js
+++ src/server/tests/SendEmailTest.js
@@ -2,7 +2,7 @@ require("dotenv").config({path: "../.env"})
 
 const EmailServices = require("../services/EmailService")
 const PasswordUtils = require("../utils/PasswordUtils")
-const data = {usuSenha: PasswordUtils.randomPassword(), usuNome: "Charles"}
-const template = "../templates/FirstPasswordTemplate.ejs"
+const data = {usuSenha: PasswordUtils.randomPassword(), usuNome: "Wesley Dias"}
+const template = "../templates/FirstAccessTemplate.ejs"
 
-EmailServices.sendEmail("BureAuto", "charles.ferreira.ramos@gmail.com", "senha gerada", template, data)
+EmailServices.sendEmail("BureAuto", "wesley.dias3@fatec.sp.gov.br", "BureAuto - Primeiro Acesso", template, data)
