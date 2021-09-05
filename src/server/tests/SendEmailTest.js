632bf2b3299425d80f9e31ae829843929af706d9
Caique Nascimento <caiquemigueldes@gmail.com>
2021-09-05 00:00:30 -0300

    troca de nome de varriaveis

--- src/server/tests/SendEmailTest.js
+++ src/server/tests/SendEmailTest.js
@@ -2,7 +2,7 @@ require("dotenv").config({path: "../.env"})
 
 const EmailService = require("../services/EmailService")
 const PasswordUtils = require("../utils/PasswordUtils")
-const data = {usuSenha: PasswordUtils.randomPassword(), usuNome: "Wesley Dias"}
+const data = {senha: PasswordUtils.randomPassword(), nome: "Seu Nome"}
 const template = "../templates/FirstAccessTemplate.ejs"
 
-EmailService.sendEmail("BureAuto", "wesley.dias3@fatec.sp.gov.br", "BureAuto - Primeiro Acesso", template, data)
+EmailService.sendEmail("BureAuto", "seu_email@email.com", "BureAuto - Primeiro Acesso", template, data)
