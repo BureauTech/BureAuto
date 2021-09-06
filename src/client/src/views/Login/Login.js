bae6cfded5f9abbd8da6be8aa2fec2a5710e1818
Denis Lima <denislima99@gmail.com>
2021-09-06 00:16:02 -0300

    Adiconando view de login

---
+++ src/client/src/views/Login/Login.js
@@ -0,0 +1,23 @@
+import Card from "@/components/Card/Card.vue"
+import Input from "@/components/Input/Input.vue"
+import Button from "@/components/Button/Button.vue"
+import rulesUtils from "@/utils/rulesUtils"
+
+export default {
+    name: "Login",
+    components: {
+        Card,
+        Input,
+        Button
+    },
+    data: function() {
+        return {
+            rules: rulesUtils
+        }
+    },
+    methods: {
+        teste: function() {
+            console.log("teste")
+        }
+    }
+}
---
+++ src/client/src/views/Login/Login.vue
@@ -0,0 +1,55 @@
+<template>
+  <Card>
+    <v-col cols="11">
+      <v-row
+        align="center"
+        justify="center"
+        no-gutters
+      >
+        <v-img
+          src="../../../../../doc/img/bureauto_sem_fundo.png"
+          alt="Logo da bureAuto"
+          max-height="200"
+          max-width="200"
+        />
+      </v-row>
+      <v-row no-gutters>
+        <p class="text-h4 bahama--text">Acesso</p>
+        <p class="text-h5 bahama--text">Insira seu e-mail e senha para fazer o login.</p>
+      </v-row>
+      <v-row no-gutters>
+        <Input
+          placeholder="exemplo@bureau.com"
+          :propsRules="[rules.required, rules.email]"
+        />
+      </v-row>
+      <v-row no-gutters>
+        <Input
+          placeholder="Senha"
+          :propsRules="[rules.required]"
+        />
+      </v-row>
+      <v-row
+        align="center"
+        justify="center"
+        class="mb-6 mt-0"
+      >
+        <Button buttonText="Login" @click="teste" />
+      </v-row>
+      <v-row
+        no-gutters
+        align="center"
+        justify="center"
+      >
+        <router-link
+          to="/forgot-password"
+          class="text-decoration-none"
+        >
+          <p class="text-h6 bahama--text">Esqueci minha senha</p>
+        </router-link>
+      </v-row>
+    </v-col>
+  </Card>
+</template>
+
+<script src="./Login.js"></script>
