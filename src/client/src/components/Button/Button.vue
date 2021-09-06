e9690d45c7c400d03efaf7f8b4312fc484bd33e8
Denis Lima <denislima99@gmail.com>
2021-09-06 00:15:06 -0300

    Adicionando componente de botão

---
+++ src/client/src/components/Button/Button.vue
@@ -0,0 +1,17 @@
+<template>
+  <v-btn
+    elevation="2"
+    rounded
+    v-text="buttonText"
+    color="bahama"
+    class="white--text text-none text-h5"
+    min-width="150"
+    min-height="50"
+    @click="emit"
+  ></v-btn>
+</template>
+
+<script src="./Button.js"></script>
+
+<style>
+</style>
