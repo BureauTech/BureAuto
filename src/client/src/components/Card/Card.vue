b372bd276b4ca5fc81e702b048cf3655b0dcaa23
Denis Lima <denislima99@gmail.com>
2021-09-06 00:15:13 -0300

    Adicionando componente de card

---
+++ src/client/src/components/Card/Card.vue
@@ -0,0 +1,26 @@
+<template>
+  <v-container fill-height>
+    <v-row
+      align="center"
+      justify="center"
+    >
+      <v-col
+        cols="12"
+        sm="6"
+        xl="4"
+      >
+        <v-card
+          elevation="1"
+          class="mx-auto my-12 rounded-xl"
+          align="center"
+          justify="center"
+        >
+          <slot></slot>
+
+        </v-card>
+      </v-col>
+    </v-row>
+  </v-container>
+</template>
+
+<script src="./Card.js"></script>
