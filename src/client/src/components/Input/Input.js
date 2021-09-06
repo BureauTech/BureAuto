499a41df3bd03e8350e50b9a00b21ced2e8f13f1
Denis Lima <denislima99@gmail.com>
2021-09-06 00:15:22 -0300

    Adicionando componente de input

---
+++ src/client/src/components/Input/Input.js
@@ -0,0 +1,9 @@
+export default {
+    name: "Input",
+    props: {
+        placeholder: String,
+        propsRules: {
+            default: ""
+        }
+    }
+}
---
+++ src/client/src/components/Input/Input.vue
@@ -0,0 +1,11 @@
+<template>
+  <v-text-field
+    outlined
+    rounded
+    :rules="propsRules"
+    :placeholder="placeholder"
+    color="bahama"
+  />
+</template>
+
+<script src="./Input.js"></script>
