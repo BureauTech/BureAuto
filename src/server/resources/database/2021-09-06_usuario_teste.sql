ad649fad42d8470c68be46bbef19729afd13a21f
Denis Lima <denislima99@gmail.com>
2021-09-06 01:28:36 -0300

    Arrumando script, adicionando usuario teste

---
+++ src/server/resources/database/2021-09-06_usuario_teste.sql
@@ -0,0 +1,3 @@
+INSERT INTO public.usuario(
+	usu_nome, usu_is_cpf, usu_documento, usu_apelido, usu_telefone, usu_endereco, usu_email, usu_senha)
+	VALUES ('dev', true, '1234567', 'devinho', '123456789', 'rua dos programadores', 'dev@dev.com', 'dev');
