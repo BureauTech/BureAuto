412e0c9dc3a02b7a4f4c05b7f1ea28b0c4c88dcc
Denis Lima <denislima99@gmail.com>
2021-09-16 22:16:53 -0300

    Arrumado opacidade do app, alterando card

--- src/client/src/views/Home/Home.vue
+++ src/client/src/views/Home/Home.vue
@@ -34,15 +34,15 @@
           min-width="100%"
           rounded="xl"
         >
-          <v-card-title v-text="ad.adv_model_description" />
-          <v-card-subtitle v-text="ad.Manufacturer.man_name" />
           <v-img
             src="@/assets/bureauto_sem_fundo.png"
             alt="Logo da bureAuto"
-            max-height="200"
-            max-width="200"
-          />
-          <v-card-text v-text="'R$ ' + ad.adv_value.toLocaleString('pt-br')" />
+            max-height="300"
+          >
+          <v-card-title v-text="ad.Manufacturer.man_name" class="text-h5 bahama--text mousehover" />
+          </v-img>
+          <v-card-subtitle v-text="ad.adv_model_description" class="text-h5" />
+          <v-card-text v-text="'R$ ' + ad.adv_value.toLocaleString('pt-br')" class="text-h5" />
           <v-btn
             elevation="0"
             color="transparent"
