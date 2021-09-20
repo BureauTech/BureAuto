4090e5d95b96463e69fd6ae19b033fe0379c6656
charles-ramos <charles.ramos@fatec.sp.gov.br>
2021-09-20 07:50:42 -0300

    Adicao mapeamento tableas do banco (favorite, message, chat)

--- src/server/database/Entities.js
+++ src/server/database/Entities.js
@@ -4,6 +4,9 @@ module.exports = {
     Advertisement: require("./models/Advertisement"),
     User: require("./models/User"),
     Manufacturer: require("./models/Manufacturer"),
+    Favorite: require("./models/Favorite"),
+    Chat: require("./models/Chat"),
+    Message: require("./models/Message"),
 
     list: function() {
         return Object.values(this)
