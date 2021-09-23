8a6cac40dabca462c5f722e33f40663b63377b00
Denis Lima <denislima99@gmail.com>
2021-09-23 11:43:43 -0300

    Serviço para relatório de qtd anúncios favoritados (anunciante)

--- src/server/services/FavoriteService.js
+++ src/server/services/FavoriteService.js
@@ -27,5 +27,30 @@ module.exports = {
     deleteFavorite: async function(fav_cod) {
         const RepositoryFavorite = await Repository.get(Repository.Favorite)
         return await RepositoryFavorite.delete(fav_cod)
+    },
+
+    getAdvertiserReport: async function(adv_use_cod) {
+        const RepositoryFavorite = await Repository.get(Repository.Favorite)
+        const allAdvertisementFavorited = await RepositoryFavorite.createQueryBuilder("favorite")
+            .distinctOn(["favorite.fav_adv_cod"])
+            .leftJoin("advertisement", "advertisement", "favorite.fav_adv_cod = advertisement.adv_cod")
+            .where("advertisement.adv_use_cod = :adv_use_cod", {adv_use_cod: adv_use_cod})
+            .getMany()
+        const advertisementFavoritedCount = allAdvertisementFavorited.length
+        
+        //  Se não tem nenhum, é 0%
+        if (!advertisementFavoritedCount) {
+            return 0
+        }
+        
+        const RepositoryAdvertisement = await Repository.get(Repository.Advertisement)
+        const allAdvertisementCount = await RepositoryAdvertisement.count({adv_use_cod: adv_use_cod})
+        
+        //  Se não tem nenhum, é 0%
+        if (!allAdvertisementCount) {
+            return 0
+        }
+
+        return advertisementFavoritedCount / allAdvertisementCount
     }
 }
