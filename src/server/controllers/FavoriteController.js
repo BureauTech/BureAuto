bfe1d65430c649aaa97f8b8327aa759a31a77efb
Denis Lima <denislima99@gmail.com>
2021-09-23 11:45:48 -0300

    Controller para relatorio de favoritos

--- src/server/controllers/FavoriteController.js
+++ src/server/controllers/FavoriteController.js
@@ -15,17 +15,6 @@ router.get("/all/:use_cod", async(req, res) => {
     }
 })
 
-router.get("/:use_cod/:adv_cod", async(req, res) => {
-    try {
-        const {use_cod, adv_cod} = req.params
-        const favorite = await FavoriteService.getFavorite(use_cod, adv_cod)
-        return res.status(200).send({success: true, data: favorite})
-    } catch (error) {
-        console.log(error)
-        return res.status(500).send({success: false, error: "an error occurred while processing the request"})
-    }
-})
-
 router.post("/register", async(req, res) => {
     try {
         const {use_cod, adv_cod} = req.body
@@ -42,6 +31,27 @@ router.delete("/:fav_cod", async(req, res) => {
         const {fav_cod} = req.params
         await FavoriteService.deleteFavorite(fav_cod)
         return res.status(200).send({success: true})
+    } catch (error) {
+        console.log(error)
+        return res.status(500).send({success: false, error: "an error occurred while processing the request"})
+    }
+})
+
+router.get("/report/:adv_use_cod", async(req, res) => {
+    try {
+        const a = await FavoriteService.getAdvertiserReport(req.params.adv_use_cod)
+        return res.status(200).send({success: true, data: a})
+    } catch (error) {
+        console.log(error)
+        return res.status(500).send({success: false, error: "an error occurred while processing the request"})
+    }
+})
+
+router.get("/:use_cod/:adv_cod", async(req, res) => {
+    try {
+        const {use_cod, adv_cod} = req.params
+        const favorite = await FavoriteService.getFavorite(use_cod, adv_cod)
+        return res.status(200).send({success: true, data: favorite})
     } catch (error) {
         console.log(error)
         return res.status(500).send({success: false, error: "an error occurred while processing the request"})
