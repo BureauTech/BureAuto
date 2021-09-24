2af08559210ee326f3b34c536f1579dcfd1156ec
Denis Lima <denislima99@gmail.com>
2021-09-24 10:23:40 -0300

    Criando componente para card de relatórios, pegando relatório de % de anúncios favoritos

---
+++ src/client/src/components/ReportCard/ReportCard.vue
@@ -0,0 +1,21 @@
+<template>
+  <v-col
+    cols="6"
+    align="center"
+  >
+    <v-card
+      min-width="100%"
+      rounded="xl"
+    >
+      <v-card-title>
+        <h3 v-text="title"></h3>
+      </v-card-title>
+      <v-card-text v-for="(item, index) in content" :key="index">
+        <p class="text-body-1">{{item.text}}<b>{{item.value}}</b> </p>
+      </v-card-text>
+    </v-card>
+  </v-col>
+</template>
+
+<script src="./ReportCard.js"></script>
+<style></style>
--- src/client/src/views/Reports/Reports.js
+++ src/client/src/views/Reports/Reports.js
@@ -1,3 +1,46 @@
+import ReportCard from "@/components/ReportCard/ReportCard.vue"
+import axios from "@/axios"
 export default {
-    name: "Reports"
+    name: "Reports",
+
+    components: {
+        ReportCard
+    },
+
+    data: function() {
+        return {
+            engagement: [{
+                text: "N. de visualizações do anúncio: ",
+                value: 300
+            }, {
+                text: "N. de pessoas que entraram em contato: ",
+                value: 20
+            }, {
+                text: "Percentual de engajamento do anúncio: ",
+                value: "6,67%"
+            }],
+            favorite: [{
+                text: "Porcentagem de anúncios favoritados: ",
+                value: ""
+            }]
+        }
+    },
+
+    methods: {
+        getFavoriteReport: async function() {
+            const {use_cod} = this.$store.getters.getUser
+            try {
+                const {data} = await axios.get(`/favorite/report/${use_cod}`)
+                if (data.success) {
+                    this.favorite[0].value = data.data
+                }
+            } catch (error) {
+                this.$toasted.error("Ocorreu um erro ao buscar o relatório de favoritos")
+            }
+        }
+    },
+
+    created: function() {
+        this.getFavoriteReport()
+    }
 }
