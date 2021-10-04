90f6a864563cf8161bdac58588e77d99d4b7ab9a
charles-ramos <charles.ramos@fatec.sp.gov.br>
2021-10-04 10:45:21 -0300

    atualizacao: remocao dos anuncios inativos

--- src/client/src/views/Favorites/Favorites.vue
+++ src/client/src/views/Favorites/Favorites.vue
@@ -1,79 +1,77 @@
 <template>
   <v-container>
     <v-row>
-    <v-app-bar
-      color="transparent"
-      elevation="0"
-      justify
-    >
-      <v-layout>
-        <v-col
-          cols="16"
-        >
-          <h3>
-            Favoritos
-          </h3>
-          Seus anúncios favoritos
-        </v-col>
-      </v-layout>
-    </v-app-bar>
-    </v-row>
-      <v-layout
-        v-for="(ad, index) in ads"
-        :key="ad.adv_cod"
-        cols="4"
-        row
-        justify-center
-        align-start
-        class="no-negative"
+      <v-app-bar
+        color="transparent"
+        elevation="0"
+        justify
       >
-        <v-card
-          min-width="75%"
-          rounded="xl"
-          align-center
-          class="text-center margin-layout"
-        >
-          <v-card-title>
-            <v-col
-              align="center"
-              cols="12"
-              v-text="ad.adv_model_description + ' - ' + ad.adv_value.toLocaleString('pt-BR', { style: 'currency', currency: 'BRL' })"
+        <v-layout>
+          <v-col cols="12">
+            <h3>
+              Favoritos
+            </h3>
+            Seus anúncios favoritos
+          </v-col>
+        </v-layout>
+      </v-app-bar>
+    </v-row>
+    <v-layout
+      v-for="(ad, index) in ads"
+      :key="ad.adv_cod"
+      cols="4"
+      row
+      justify-center
+      align-start
+      class="no-negative"
+    >
+      <v-card
+        min-width="75%"
+        rounded="xl"
+        align-center
+        class="text-center margin-layout"
+      >
+        <v-card-title>
+          <v-col
+            align="right"
+            cols="11"
+            v-text="ad.adv_model_description + ' - ' + ad.adv_value.toLocaleString('pt-BR', { style: 'currency', currency: 'BRL' })"
+          >
+          </v-col>
+          <v-col align="right">
+            <v-icon
+              large
+              @click="deleteFav(ad.adv_cod, index)"
             >
-            </v-col>
-            <v-col align="right">
-              <v-icon
-                large
-                @click="deleteFav(ad.adv_cod, index)"
-              >
-                mdi-trash-can-outline
-              </v-icon>
-            </v-col>
-          </v-card-title>
-          <v-row>
-            <v-col
-              cols="4"
-              align="center"
-            >
-              <v-card-text>
-                <v-img
-                  src="@/assets/bureauto_sem_fundo.png"
-                  alt="Logo da bureAuto"
-                  max-height="150"
-                  max-width="150"
-                />
-              </v-card-text>
-            </v-col>
-            <v-col
-              align="justify"
-              max-width="75%"
-              cols="8"
-            >
-              <v-list-item v-text="ad.adv_brand_description">
-              </v-list-item>
-            </v-col>
-          </v-row>
-        </v-card>
-      </v-layout>
+              mdi-trash-can-outline
+            </v-icon>
+          </v-col>
+        </v-card-title>
+        <v-row>
+          <v-col
+            cols="6"
+            align="center"
+            justify="center"
+          >
+            <v-card-text>
+              <v-img
+                src="@/assets/bureauto_sem_fundo.png"
+                alt="Logo da bureAuto"
+                max-height="150"
+                max-width="150"
+              />
+            </v-card-text>
+          </v-col>
+          <v-col
+            max-width="75%"
+            cols="6"
+          >
+            <v-list-item v-text="ad.adv_brand_description">
+            </v-list-item>
+          </v-col>
+        </v-row>
+      </v-card>
+    </v-layout>
   </v-container>
 </template>
 
