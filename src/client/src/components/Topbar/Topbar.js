c6d5344a0884c98d27dbc393f09cf8ca4a4ec4c2
Denis Lima <denislima99@gmail.com>
2021-10-06 10:56:48 -0300

    Responsividade no menu

--- src/client/src/components/Topbar/Topbar.js
+++ src/client/src/components/Topbar/Topbar.js
@@ -7,5 +7,49 @@ export default {
             await axios.get("/logout")
             window.location.href = "/login"
         }
+    },
+    data: function() {
+        return {
+            drawer: false,
+            group: null,
+            links: [{
+                path: "/cadastrar/anuncio",
+                requiresAdmin: false,
+                text: "Anunciar"
+            }, {
+                path: "/",
+                requiresAdmin: false,
+                text: "Catálogo"
+            }, {
+                path: "/relatorios",
+                requiresAdmin: false,
+                text: "Relatórios"
+            }, {
+                path: "/favoritos",
+                requiresAdmin: false,
+                text: "Favoritos"
+            }, {
+                path: "/cadastrar/usuario",
+                requiresAdmin: true,
+                text: "Usuários"
+            }, {
+                path: "/perfil",
+                requiresAdmin: false,
+                text: "Perfil"
+            }]
+        }
+    },
+    computed: {
+        linksDisponiveis() {
+            return this.links.filter((link) => {
+                const {route} = this.$router.resolve(link.path)
+
+                return (
+                    !route.matched.some((record) => record.meta.requiresAdmin) ||
+          this.$store.getters.getUser.use_is_admin
+                )
+            })
+        }
+
     }
 }
--- src/client/src/components/Topbar/Topbar.vue
+++ src/client/src/components/Topbar/Topbar.vue
@@ -5,7 +5,7 @@
       align="center"
       class="bahama--text"
     >
-      <v-col>
+      <v-col class="d-none d-md-flex">
         <router-link
           to="/"
           class="text-decoration-none"
@@ -18,62 +18,78 @@
           />
         </router-link>
       </v-col>
-      <v-col>
+      <v-col
+        v-for="link in linksDisponiveis"
+        :key="link.path"
+      >
         <router-link
-          to="/cadastrar/anuncio"
-          class="text-decoration-none"
+          :to="link.path"
+          class="text-decoration-none d-none d-sm-flex"
+          v-text="link.text"
         >
-          Anunciar
-        </router-link>
-      </v-col>
-      <v-col>
-        <router-link
-          to="/"
-          class="text-decoration-none"
-        >
-          Catálogo
-        </router-link>
-      </v-col>
-      <v-col>
-        <router-link
-          to="/relatorios"
-          class="text-decoration-none"
-        >
-          Relatórios
-        </router-link>
-      </v-col>
-      <v-col>
-        <router-link
-          to="/favoritos"
-          class="text-decoration-none"
-        >
-          Favoritos
-        </router-link>
-      </v-col>
-      <v-col v-if="$store.getters.getUser.use_is_admin">
-        <router-link
-          to="/cadastrar/usuario"
-          class="text-decoration-none"
-        >
-          Usuários
-        </router-link>
-      </v-col>
-      <v-col v-if="$store.getters.isAuthenticated">
-        <router-link
-          to="/perfil"
-          class="text-decoration-none"
-        >
-          Perfil
         </router-link>
       </v-col>
       <v-col>
         <a
-          class="text-decoration-none"
+          class="text-decoration-none d-none d-sm-flex"
           @click="logout"
           v-text="$store.getters.isAuthenticated ? 'Logout' : 'Login'"
         />
       </v-col>
+      <v-app-bar-nav-icon
+        @click.stop="drawer = !drawer"
+        class="d-sm-none"
+      ></v-app-bar-nav-icon>
     </v-row>
+
+    <v-navigation-drawer
+      v-model="drawer"
+      absolute
+      temporary
+      width="150"
+    >
+      <v-list
+        nav
+        dense
+      >
+        <v-list-item-group
+          v-model="group"
+          active-class="deep-purple--text text--accent-4"
+        >
+          <v-list-item
+            to="/"
+            class="text-decoration-none"
+          >
+            <v-img
+              src="@/assets/bureauto_sem_fundo.png"
+              alt="Logo da bureAuto"
+              max-height="100"
+              max-width="100"
+            />
+          </v-list-item>
+          <v-list-item
+            v-for="link in linksDisponiveis"
+            :key="link.path"
+            :to="link.path"
+            class="text-decoration-none d-sm-none"
+          >
+            <v-list-item-title>
+              <p
+                v-text="link.text"
+                class="text-h6 font-weight-regular"
+              ></p>
+            </v-list-item-title>
+          </v-list-item>
+          <v-list-item>
+            <v-list-item-title @click="logout">
+              <a class="text-decoration-none black--text d-sm-none text-h6 font-weight-regular">
+                <p v-text="$store.getters.isAuthenticated ? 'Logout' : 'Login'"></p>
+              </a>
+            </v-list-item-title>
+          </v-list-item>
+        </v-list-item-group>
+      </v-list>
+    </v-navigation-drawer>
   </v-container>
 </template>
 
