e898a04a706286548691a5816d292b3acaf495b6
Daniel Santos Oliveira <daniel.oliveira96@fatec.sp.gov.br>
2021-10-06 08:12:36 -0300

    Correção do script.

--- src/server/resources/database/UpdateDB.js
+++ src/server/resources/database/UpdateDB.js
@@ -3,10 +3,12 @@ const fs            = require("fs")
 
 // Instalar a biblioteca pg || npm install -save pg
 // Se atentar as configurações do DB antes de executar o script.
+// Esse script só executa para a base <bureauto> já criada.
 const config = {
     host: "localhost",
     user: "postgres",
     password: "postgres",
+    database: "bureauto",
     port: 5432
 }
 
