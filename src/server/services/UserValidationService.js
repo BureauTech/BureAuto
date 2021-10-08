f3c10667420a8712f455f288d367c3d62edd95df
WesVisiona <wesley.dias@visionaespacial.com.br>
2021-10-08 17:35:16 -0300

    Adição validação de documento já cadastrado no sistema

--- src/server/services/UserValidationService.js
+++ src/server/services/UserValidationService.js
@@ -6,11 +6,15 @@ module.exports = {
     validateUser: async function(user) {
         const connection = await Connection
         
-        const statusDocument = ValidationUtils.validDocument(user.documento)
-        user.tipoDocumento = statusDocument.type
-
-        if (!statusDocument.valid) {
+        const newDocument = ValidationUtils.validDocument(user.documento)
+        user.tipoDocumento = newDocument.type
+        if (!newDocument.valid) {
             return {valid: false, error: "documento inválido"}
+        }
+
+        const document = (await connection.query("select * from user_document_exists($1) exists", [user.documento]))[0]
+        if (document.exists) {
+            return {valid: false, error: "documento já cadastrado na plataforma"}
         }
 
         const email = (await connection.query("select * from user_email_exists($1) exists", [user.email]))[0]
