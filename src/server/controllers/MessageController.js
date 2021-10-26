81be4a32f100ebc43a7b05bcee6908d49ef8c204
charles-ramos <charles.ramos@fatec.sp.gov.br>
2021-10-26 10:37:50 -0300

    ajustes nas mensagens de erro

--- src/server/controllers/MessageController.js
+++ src/server/controllers/MessageController.js
@@ -9,7 +9,7 @@ router.post("/create", async(req, res) => {
         const {cha_cod, message} = req.body
         const newMessage = await MessageService.createMessage(req.user.use_cod, cha_cod, message)
         if (newMessage) return res.status(201).send({success: true, data: newMessage})
-        return res.status(400).send({sucess: false, error: "you haven't permission to send message in this chat"})
+        return res.status(400).send({sucess: false, error: "you don't have permission to send message in this chat"})
     } catch (error) {
         console.log(error)
         return res.status(500).send({sucess: false, error: "an error occurred while processing the request"})
