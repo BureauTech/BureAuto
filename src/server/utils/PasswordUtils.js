d05a17080e946bc3ed675521fd676e35d7eedb63
Denis Lima <denislima99@gmail.com>
2021-10-30 11:05:19 -0300

    Verificando senha forte no server

--- src/server/utils/PasswordUtils.js
+++ src/server/utils/PasswordUtils.js
@@ -12,6 +12,11 @@ module.exports = {
                 {characters: password.lower, exactly: 4}
             ]
         })
+    },
+
+    isStrongPassword: function(password) {
+        // length between [8, 16], at least 1 upper case, 1 lower case, 1 number, 1 symbol
+        return password.match(/^(?=.*[A-Z])(?=.*[!@#$&*\.%])(?=.*[0-9])(?=.*[a-z]).{8,16}$/g)
     }
 
 }
