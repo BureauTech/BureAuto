4b4e99c43ba72df1f753a3b8760f2699eb6af2ee
charles-ramos <charles.ramos@fatec.sp.gov.br>
2021-11-24 09:33:45 -0300

    adição SSL - conexão banco

--- database/Connection.js
+++ database/Connection.js
@@ -1,5 +1,5 @@
-const typeorm = require("typeorm")
-const Entities = require("./Entities")
+const typeorm = require(typeorm)
+const Entities = require(./Entities)
 
 const Connection = typeorm.createConnection({
     type: process.env.TYPE_DB,
@@ -8,7 +8,13 @@ const Connection = typeorm.createConnection({
     username: process.env.USER_DB,
     password: process.env.PWD_DB,
     database: process.env.DATABASE_DB,
-    entities: Entities.list()
+    entities: Entities.list(),
+    ssl: true,
+    extra: {
+        ssl: {
+            rejectUnauthorized: false
+        }
+    }
 })
 
 module.exports = Connection
