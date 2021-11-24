8fdaa95b2d1867c1e1c21f21fd611ec12dc967cb
charles-ramos <charles.ramos@fatec.sp.gov.br>
2021-11-24 11:12:06 -0300

    ajustes para salvar cookie

--- controllers/LogoutController.js
+++ controllers/LogoutController.js
@@ -5,6 +5,7 @@ const AuthService = require("../services/AuthService")
 
 router.get("/", async(req, res) => {
     res.cookie(AuthService.cookieName, "", {httpOnly: true, maxAge: AuthService.expiresIn * 1000, sameSite: "none", secure: true})
+    res.clearCookie(AuthService.cookieName)
     return res.status(200).send({success: true})
 })
 
